f52ed8fc1bccf0a816ac8bf2b197efb229e1e66a
robertramosastudillo <robertramosastudillo@gmail.com>
2021-02-20 15:43:57 -0500

    Se trabajo en la implementacion de express-validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,14 +3,7 @@ const { response } = require("express");
 const crearUsuario = (req, res = response) => {
   const { name, email, password } = req.body;
 
-  if (name.length < 5) {
-    return res.status(400).json({
-      ok: false,
-      msg: "El nombre debe de ser de 5 letras",
-    });
-  }
-
-  res.json({
+  res.status(201).json({
     ok: true,
     msg: "register",
     name,
--- routes/auth.js
+++ routes/auth.js
@@ -4,17 +4,40 @@ host + /api/auth
 */
 
 const { Router } = require("express");
-const router = Router();
-
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
 const {
   crearUsuario,
   loginUsuario,
   revalidarToken,
 } = require("../controllers/auth");
+const router = Router();
 
-router.post("/new", crearUsuario);
+router.post(
+  "/new",
+  [
+    // Middlewares
+    check("name", "El nombre es obligatorio").notEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+    validarCampos,
+  ],
+  crearUsuario
+);
 
-router.post("/", loginUsuario);
+router.post(
+  "/",
+  [
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+    validarCampos,
+  ],
+  loginUsuario
+);
 
 router.get("/renew", revalidarToken);
 
