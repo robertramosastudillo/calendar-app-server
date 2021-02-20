54336b1cac3321da9daf297faa56e4ed812edb34
robertramosastudillo <robertramosastudillo@gmail.com>
2021-02-20 15:04:45 -0500

    Se empezo a tabajar en la estructura del proyecto

---
+++ controllers/auth.js
@@ -0,0 +1,44 @@
+const { response } = require("express");
+
+const crearUsuario = (req, res = response) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: "El nombre debe de ser de 5 letras",
+    });
+  }
+
+  res.json({
+    ok: true,
+    msg: "register",
+    name,
+    email,
+    password,
+  });
+};
+
+const loginUsuario = (req, res = response) => {
+  const { email, password } = req.body;
+
+  res.json({
+    ok: true,
+    meg: "Login",
+    email,
+    password,
+  });
+};
+
+const revalidarToken = (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "renew",
+  });
+};
+
+module.exports = {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+};
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+
+// Crear el servidor de express
+
+const app = express();
+
+// Directorio Público
+app.use(express.static("public"));
+
+// Lectura y parseo del body
+app.use(express.json());
+
+// Rutas
+app.use("/api/auth", require("./routes/auth"));
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
